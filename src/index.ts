fc55e61af73462b7b1f2c6a2622023079c8e56ec
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-21 09:21:39 +0000

    Print out all of the configs

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,21 @@ const { writeFile, readdir, readFile } = require("fs").promises;
 
 const configFiles: any = {};
 const configFolderPath = path.resolve(__dirname, "config");
+
+(async () => {
+	const files = await readdir(configFolderPath).catch(console.log);
+
+	for (let i of files) {
+		const licenseName = i.split(".")[0];
+		configFiles[licenseName] = path.join(configFolderPath, i);
+	}
+
+	const { LICENSE } = await inquirer.prompt([
+		{
+			type: "list",
+			message: "Which LICENSE do you want to generate?",
+			name: "license",
+			choices: Object.keys(configFiles),
+		},
+	]);
+})();
