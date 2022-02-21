dc27b9c670d94675bde6bd545518c1acc60fe6c7
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-21 09:18:51 +0000

    Imported Configs

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const { writeFile, readdir, readFile } = require("fs").promises;
+
+const configFiles: any = {};
+const configFolderPath = path.resolve(__dirname, "config");
